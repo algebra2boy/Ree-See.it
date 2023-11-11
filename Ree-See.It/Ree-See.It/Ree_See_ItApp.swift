6e9ef54adfded2a4defe3ed0afa6cb724dc26d54
Yongye Tan <yongyetan1209@gmail.com>
2023-11-10 23:09:23 -0500

    Initial commit

---
+++ Ree-See.It/Ree-See.It/Ree_See_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ree_See_ItApp.swift
+//  Ree-See.It
+//
+//  Created by Yongye Tan on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Ree_See_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
