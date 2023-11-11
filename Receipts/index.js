82d799347b50ea9246398bf69b5ed3afd5767fa6
Jeff L <jjl01332@gmail.com>
2023-11-11 13:18:59 -0500

    MongoDB connection established

--- Receipts/index.js
+++ Receipts/index.js
@@ -1,10 +1,25 @@
 import express from 'express'
 import router from './routes/routes.js'
+import client from './modules/db_connect.js'
 
 const app = express()
 
 app.use(router)
 
-app.listen(3000, () => {
+async function go(){
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+app.listen(54102, () => {
   console.log('Server started on port 3000')
+  go().then(() => console.log("AHA"));
 })
---
+++ Receipts/modules/db_connect.js
@@ -0,0 +1,15 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import dotenv from 'dotenv';
+dotenv.config();
+const pass = process.env.PASSKEY;
+const uri = `mongodb+srv://Reesee-it:${pass}@cluster0.m03sciv.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+export default client;
