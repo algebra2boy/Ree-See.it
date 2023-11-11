7f45f13dcb90d671b48449f84a3eb320b3abda21
Eric Huang <ejhuang@umass.edu>
2023-11-11 12:46:50 -0500

    basic comments

--- geo-service/app.py
+++ geo-service/app.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 def get_geocode():
     address = request.args.get('address')
     
+    # if address is not provided with status 400
     if address is None:
         return "address not found", 400
     
+    # getcode the address, if invalid return an invalid message with status 400
     try:
         coordinate = ox.geocode(address)
     except ValueError:
@@ -20,7 +22,9 @@ def get_geocode():
         "long": coordinate[1]
     }}
     
+    # success return json with status 200
     return jsonify(geoData), 200
 
+# set to port 3000 for now since mac uses port 5000
 if __name__ == "__main__":
     app.run(debug=True, port=3000, host="0.0.0.0")
