412d8737a41c845717037651e570e45a1291ab26
Eric Huang <ejhuang@umass.edu>
2023-11-11 12:45:14 -0500

    error checks and docker file

--- geo-service/app.py
+++ geo-service/app.py
@@ -6,7 +6,15 @@ app = Flask(__name__)
 @app.route('/get-geocode')
 def get_geocode():
     address = request.args.get('address')
-    coordinate = ox.geocode(address)
+    
+    if address is None:
+        return "address not found", 400
+    
+    try:
+        coordinate = ox.geocode(address)
+    except ValueError:
+        return "invalid address", 400
+    
     geoData = {"coordinate": {
         "lat": coordinate[0],
         "long": coordinate[1]
@@ -15,4 +23,4 @@ def get_geocode():
     return jsonify(geoData), 200
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=3000, host="0.0.0.0")
