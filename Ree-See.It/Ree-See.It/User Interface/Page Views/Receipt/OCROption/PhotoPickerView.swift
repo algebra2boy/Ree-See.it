0aea82ffa52b659dffac548519e1f93329fe3815
Yongye Tan <yongyetan1209@gmail.com>
2023-11-11 14:01:26 -0500

    Adding a photo feature ocr and view model

---
+++ Ree-See.It/Ree-See.It/User Interface/Page Views/Receipt/OCROption/PhotoPickerView.swift
@@ -0,0 +1,99 @@
+//
+//  PhotoPickerView.swift
+//  Ree-See.It
+//
+//  Created by Yongye Tan on 11/11/23.
+//
+
+import SwiftUI
+import PhotosUI
+import SwiftUI
+
+struct PhotoPickerView: View {
+    
+    @State private var photoItem: PhotosPickerItem?
+    @State private var image: Image?
+    
+    @StateObject private var photoNetworkViewModel = PhotoNetworkViewModel()
+    
+    @State private var isAlertShown: Bool = false
+
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+                if let image {
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 800)
+                }
+                
+                
+                PhotosPicker("Select image",
+                             selection: $photoItem,
+                             matching: .images)
+                
+                
+                if let image {
+                    Button {
+                        uploadImage()
+                    } label: {
+                        Text("Starting scanning using our OCR")
+                    }
+                    .frame(maxWidth: .infinity)
+                    .buttonStyle(.borderedProminent)
+                }
+                
+            
+            }
+            .onChange(of: photoItem) {
+                Task {
+                    
+                    do {
+                        guard let data = try await photoItem?.loadTransferable(type: Data.self) else {
+                            return
+                        }
+                        
+                        guard let uiImage = UIImage(data: data) else {
+                            return
+                        }
+                        
+                        image = Image(uiImage: uiImage)
+                        
+                    } catch {
+                        print("something is wrong selecting an picture")
+                    }
+                    
+                }
+            }
+            .navigationTitle("OCR Scanning")
+        }
+    }
+    
+    func uploadImage() {
+        
+        
+        guard let url = URL(string: "http://localhost:3000/api") else {
+            return
+        }
+        
+        // specify a url request to send to Node.js server
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "POST"
+        
+        // create boundary
+        let boundary = photoNetworkViewModel.generateBoundary()
+        
+        // set content type
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        
+        
+    }
+    
+}
+
+#Preview {
+    PhotoPickerView()
+}
