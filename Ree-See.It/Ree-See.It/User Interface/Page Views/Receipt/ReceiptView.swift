c637dae4fbb15801830866ef465719f47f241dce
Chengtao <chengtaolinctl@gmail.com>
2023-11-12 01:24:44 -0500

    finish some XCode werid complier issue

--- Ree-See.It/Ree-See.It/User Interface/Page Views/Receipt/ReceiptView.swift
+++ Ree-See.It/Ree-See.It/User Interface/Page Views/Receipt/ReceiptView.swift
@@ -8,11 +8,10 @@
 import SwiftUI
 import Auth0
 
+
 struct ReceiptView: View {
     @State private var receipts: [Receipt] = [.receipt1, .receipt2, .receipt3, .receipt4, .receipt5]
-    
     @State private var filteredReceipts: [Receipt] = []
-    
     @State private var hasItemDeleted: Bool = false
     @State private var toBeDeleted: IndexSet?
     @State private var isLogoPressed: Bool = false
@@ -38,74 +37,71 @@ struct ReceiptView: View {
                                 } else {
                                     ReceiptCardView(receipt: receipt)
                                 }
+                            }
                         }
                         .onDelete(perform: deleteReceipt)
                         .listRowBackground(
-                            RoundedRectangle(
-                                cornerSize: CGSize(width: 20, height: 10))
-                            .fill(Color(white: 1, opacity: 0.8))
-                            .padding(3)
+                            RoundedRectangle(cornerSize: CGSize(width: 20, height: 10))
+                                .fill(Color(white: 1, opacity: 0.8))
+                                .padding(3)
                         )
-                        .listRowSeparator(.hidden) // hide the line
+                        .listRowSeparator(.hidden)
                     }
                 }
-                    .searchable(text: $searchText, prompt: "Search for receipt")
-                    .onChange(of: searchText) {
-                        filterReceipt()
-                    }
-                    .navigationTitle("Receipts")
-                    .alert("Do you want to delete this receipt on the cloud?", isPresented: $hasItemDeleted) {
-                        Button("Cancel", role: .cancel) { }
-                        Button("Delete", role: .destructive) {
-                            receipts.remove(atOffsets: toBeDeleted!)
-                        }
-                    }
-                    .toolbar {
-                        ToolbarItem(placement: .navigationBarLeading) {
-                            NavigationLink {
-                                SignLoginView()
-                            } label: {
-                                HStack {
-                                    UserAsyncImage(imageUrl: authManager.user?.picture, width: 40, height: 40)
-                                    
-                                    Text("Hi, \(authManager.user?.name ?? "welcome to Ree See it")")
-                                }
-                            }
-                        }
-                        ToolbarItem(placement: .navigationBarTrailing) {
-                            NavigationStack {
-                                Menu {
-                                    
-                                    SelectionMenu()
-                                    
-                                } label: {
-                                    Image(systemName: "plus")
-                                        .font(.title)
-                                        .foregroundStyle(.black)
-                                }
-                            }
-                        }
-                    }
             }
-            
-        }
-        
-        func deleteReceipt(at offsets: IndexSet) {
-            receipts.remove(atOffsets: offsets)
-        }
-        
-        func filterReceipt() {
-            filteredReceipts = receipts.filter { $0.name.localizedCaseInsensitiveContains(searchText.lowercased()) ||
-                $0.category.localizedCaseInsensitiveContains(searchText.lowercased())
+            .searchable(text: $searchText, prompt: "Search for receipt")
+            .onChange(of: searchText) { _ in
+                filterReceipt()
+            }
+            .navigationTitle("Receipts")
+            .alert("Do you want to delete this receipt on the cloud?", isPresented: $hasItemDeleted) {
+                Button("Cancel", role: .cancel) { }
+                Button("Delete", role: .destructive) {
+                    if let offsets = toBeDeleted {
+                        receipts.remove(atOffsets: offsets)
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    NavigationLink {
+                        SignLoginView()
+                    } label: {
+                        HStack {
+                            UserAsyncImage(imageUrl: authManager.user?.picture, width: 40, height: 40)
+                            Text("Hi, \(authManager.user?.name ?? "welcome to Ree See it")")
+                        }
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        // Define your action for the button here.
+                    }) {
+                        Image(systemName: "plus")
+                            .font(.title)
+                            .foregroundStyle(.black)
+                    }
+                }
             }
         }
-        
+    }
+
+    func deleteReceipt(at offsets: IndexSet) {
+        toBeDeleted = offsets
+        hasItemDeleted = true
+    }
+
+    func filterReceipt() {
+        filteredReceipts = receipts.filter { $0.name.localizedCaseInsensitiveContains(searchText.lowercased()) ||
+            $0.category.localizedCaseInsensitiveContains(searchText.lowercased())
+        }
     }
 }
 
 
 
-//#Preview {
-//    ReceiptView()
-//        .environmentObject(AuthenticationManager())
-//}
+
+#Preview {
+    ReceiptView()
+        .environmentObject(AuthenticationManager())
+}
