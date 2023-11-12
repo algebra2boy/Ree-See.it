0d75a4ce160481369a5e3377948bc3e06263f309
Yongye Tan <yongyetan1209@gmail.com>
2023-11-12 02:55:21 -0500

    `

--- Ree-See.It/Ree-See.It/User Interface/Page Views/Graph/ChartsView.swift
+++ Ree-See.It/Ree-See.It/User Interface/Page Views/Graph/ChartsView.swift
@@ -8,21 +8,37 @@
 import SwiftUI
 import Charts
 
-struct ChartsView: View {
-    let receipts: [Receipt] = [.receipt1, .receipt2, .receipt3, .receipt4, .receipt5]
-    var body: some View {
-        Chart(receipts, id: \.id) { receipt in
-            SectorMark(angle: .value("Category", receipt.category),
-                       innerRadius: .ratio(0.618),
-                       angularInset: 1.5)
-            .cornerRadius(5)
-            .opacity(receipts.mostExpensiveCategory()?.category == receipt.category ? 1: 0.3)
-            .foregroundStyle(by: .value("Category", receipt.category.))
-            )
-        }
-    }
-}
+//struct ChartsView: View {
+//    let receipts: [Receipt] = [.receipt1, .receipt2, .receipt3, .receipt4, .receipt5]
+//    var body: some View {
+//        Chart {
+//            ForEach(receipts, id: \.id) { receipt in
+//                SectorMark(angle: .value("Category", receipt.totalPrice),
+//                           innerRadius: .ratio(0.618),
+//                           angularInset: 1.5
+//                )
+//                .cornerRadius(5)
+//                .opacity(receipts.mostExpensiveCategory()?.category == receipt.category ? 1 : 0.3)
+//                .foregroundStyle(by: .value("Category", receipt.category))
+//            }
+//        }
+//        .aspectRatio(1, contentMode: .fit)
+//        .chartLegend(position: .bottom, spacing: 20)
+////        .chartBackground { chartProxy in
+////            GeometryReader(content: {geometry in
+////                let frame = geometry[chartProxy.plotFrame!]
+////                let visibleSize = min(frame.width, frame.height)
+////                
+////                if let mostExpe
+////                Text("Content \(frame.midX)")
+////                    .position(x: frame.midX, y: frame.minY)
+////            })
+////        }
+//    }
+//    
+//}
 
-#Preview {
-    ChartsView()
-}
+
+//#Preview {
+//    ChartsView()
+//}
