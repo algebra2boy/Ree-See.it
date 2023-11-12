98bc87929d0c56c334bb0906209573d0e9032881
Yongye Tan <yongyetan1209@gmail.com>
2023-11-11 20:34:19 -0500

    Recomminit

---
+++ Tesseract-ImageProcess/server/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+import expressWinston from "express-winston";
+import * as dotenv from 'dotenv';
+import logger from "../logs/logger.js";
+import imageProcessingAPI from "../Routers/imageProcessingAPI.js"
+
+
+dotenv.config()  // load environment variables 
+
+const app = express();
+const PORT = 3002;
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use(expressWinston.logger({
+    winstonInstance: logger,
+    statusLevels: true
+}));
+
+app.use("/api/image", imageProcessingAPI);
+
+app.listen(PORT, () => {
+    logger.info(`running the server on port ${PORT}`);
+});
