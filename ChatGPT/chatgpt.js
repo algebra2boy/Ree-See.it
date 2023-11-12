54822946da50b1983f5a808e7fd19f1a2f133b05
Yongye Tan <yongyetan1209@gmail.com>
2023-11-11 20:46:49 -0500

    basic chatgpt API call is done

--- ChatGPT/chatgpt.js
+++ ChatGPT/chatgpt.js
@@ -1,4 +1,61 @@
+import express from "express";
+import morgan from "morgan"
 import axios from "axios";
 import * as dotenv from 'dotenv';
 
+
+
 dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+
+let parsed_strings = "";
+let content = ""
+
+// Configure the headers for your POST request.
+const config = {
+    headers: {
+        'Authorization': `Bearer ${process.env.openaiApiKey}`,
+        'Content-Type': 'application/json'
+    }
+};
+
+// set up post Data
+const postData = {
+    // very great gpt 4
+    model: 'gpt-4-1106-preview',
+    response_format: {
+        // must use to enable json
+        type: "json_object",
+    },
+    messages: [
+        { role: 'user', content: content + parsed_strings }
+    ]
+};
+
+const sendPostRequest = async () => {
+    try {
+        const response = await axios.post('https://api.openai.com/v1/chat/completions', postData, config);
+        console.log('Response from OpenAI:', JSON.stringify(response.data));
+
+        res.status(200).json(response.data.choices[0]["message"]["content"]);
+
+    } catch (err) {
+
+        res.status(500).json({ "message": "fail to call GPT4" });
+    }
+};
+
+app.post("/api/gpt/json", async (req, res) => {
+    const receiptStrings = req.body.receiptString;
+    parsed_strings = receiptStrings;
+
+    const response = sendPostRequest();
+    return response;
+})
+
+app.listen(3005, () => "gpt is listensing on port 3005");
